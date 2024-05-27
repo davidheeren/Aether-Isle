492063d51a6d5add3d3d894cf08575e0fa596de0
davidheeren <131043498+davidheeren@users.noreply.github.com>
2024-05-26 22:59:31 -0500

    initial commit
    A mostly black project

---
+++ Aether Isle/Assets/_Project/Scripts/Utilities/SingletonBase.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace Utilities
+{
+    public class SingletonBase<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+    
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = FindFirstObjectByType<T>();
+                if (_instance == null)
+                {
+                    //Debug.Log("Created Singleton of type: " + typeof(T));
+                    GameObject obj = new GameObject(typeof(T) + " Singleton");
+                    _instance = obj.AddComponent<T>();
+                }
+    
+                return _instance;
+            }
+        }
+    
+        /*
+        public static T RawInstance
+        {
+            get
+            {
+                if ((object)_instance == null) Debug.LogError("Should not be using destroyed instance unless you know for sure there will be one");
+                return _instance;
+            }
+        }
+        */
+    }
+}
---
+++ Aether Isle/Assets/_Project/Scripts/Utilities/Timer.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+
+namespace Utilities
+{
+    public class Timer
+    {
+        public float delay { get; private set; }
+
+        float timeAtStart;
+
+        public bool isPaused { get; private set; }
+        bool wasPaused;
+
+        float timeAtPause;
+        float timeAtResume;
+
+        float totalPauseTime = 0; // added up pause time of previous pauses but not the current one
+    
+        public Timer(float delay)
+        {
+            timeAtStart = Time.time;
+            this.delay = delay;
+
+            if (delay <= 0)
+                Debug.LogError("Delay cannot be less than or equal to 0");
+        }
+
+        float currentPauseDeltaTime
+        {
+            get
+            {
+                if (isPaused)
+                    return Time.time - timeAtPause;
+                else if (wasPaused)
+                    return timeAtResume - timeAtPause;
+                else
+                    return 0;
+            }
+        }
+
+        public float currentDeltaTime
+        {
+            get
+            {
+                // only clamp this value
+                float deltaTime = timeAtStart + delay - Time.time;
+                return Mathf.Max(deltaTime + currentPauseDeltaTime + totalPauseTime, 0);
+            }
+        }
+        public float currentPercent // starts at 1 and goes to 0
+        {
+            get
+            {
+                return currentDeltaTime / delay;
+            }
+        }
+
+        public bool isDone
+        {
+            get
+            {
+                return currentDeltaTime == 0;
+            }
+        }
+        
+        public void Reset()
+        {
+            timeAtStart = Time.time;
+            isPaused = false;
+            wasPaused = false;
+            totalPauseTime = 0;
+        }
+
+        public void Stop()
+        {
+            timeAtStart = Mathf.Infinity;
+        }
+        public void Pause()
+        {
+            if (isPaused)
+            {
+                Debug.LogWarning("Paused Timer while already paused");
+                return;
+            }
+
+            if (wasPaused) // incudes previous pause times
+                totalPauseTime += timeAtResume - timeAtPause;
+
+            isPaused = true;
+            wasPaused = true;
+            timeAtPause = Time.time;
+        }
+        public void Resume()
+        {
+            if (!isPaused)
+            {
+                Debug.LogWarning("Resumed Timer while already resumed");
+                return;
+            }
+
+            isPaused = false;
+            timeAtResume = Time.time;
+        }
+    }
+}
